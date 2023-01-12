77a694e91c03a089c5f70298599437b4c084850b
Roman Titov <46985629+stat1q@users.noreply.github.com>
2023-01-12 17:56:55 +0700

    Delete DO4_LinuxMonitoring_v2.0-0 directory

--- DO4_LinuxMonitoring_v2.0-0/src/06/main.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#sudo apt-get install goaccess
-#goaccess -a -d -f /home/student/projects/DO4_LinuxMonitoring_v2.0-0/src/04/23Apr22.log -p /home/student/projects/DO4_LinuxMonitoring_v2.0-0/src/06/goaccess.conf -o /home/student/projects/DO4_LinuxMonitoring_v2.0-0/src/06/go-access.html
-
-goaccess ../04/*.log --config-file=goaccess.conf
-goaccess ../04/*.log -p goaccess.conf -o go-access.html
-
--- DO4_LinuxMonitoring_v2.0-0/src/09/main.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo chmod +x node.sh
-
-while sleep 3
-do
-    ./node.sh > /var/www/node/metrics.txt
-done
